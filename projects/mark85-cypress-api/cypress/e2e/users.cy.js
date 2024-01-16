fa75ae45d3df6d391bf113adf2a27efcf42b1168
Lucas <lucasantunes11@unesc.net>
2024-01-16 14:42:25 -0300

    Added step-by-step description of the code

--- projects/mark85-cypress-api/cypress/e2e/users.cy.js
+++ projects/mark85-cypress-api/cypress/e2e/users.cy.js
@@ -6,10 +6,9 @@ describe("POST /users", () => {
       password: "96523345",
     };
     cy.task("deleteUser", user.email)
-    cy.postUser(user)
-      .then(response => {
-        expect(response.status).to.eq(200)
-      })
+    cy.postUser(user).then(response => {
+      expect(response.status).to.eq(200)
+    });
   })
   it("duplicate email", () => {
     const user = {
@@ -19,11 +18,44 @@ describe("POST /users", () => {
     };
     cy.task("deleteUser", user.email)
     cy.postUser(user)
-    cy.postUser(user)
-      .then(response => {
-        const {message} = response.body
-        expect(response.status).to.eq(409)
-        expect(message).to.eq('Duplicated email!')
+    cy.postUser(user).then(response => {
+      const { message } = response.body
+      expect(response.status).to.eq(409)
+      expect(message).to.eq("Duplicated email!")
+    });
+  })
+  context("required fields", () => {
+    let user
+    beforeEach(() => {
+      user = {
+        name: "Lucas Antunes",
+        email: "lucas@gmail.com",
+        password: "pwd123"
+      }
+    })
+    it("name is required", () => {
+      delete user.name;
+      cy.postUser(user)
+        .then(response => {
+        expect(response.status).to.eq(400)
+        expect(response.body.message).to.eq('ValidationError: \"name\" is required')
       })
+    })
+    it("email is required", () => {
+      delete user.email;
+      cy.postUser(user)
+        .then(response => {
+        expect(response.status).to.eq(400)
+        expect(response.body.message).to.eq('ValidationError: \"email\" is required')
+      })
+    })
+    it("password is required", () => {
+      delete user.password;
+      cy.postUser(user)
+        .then(response => {
+        expect(response.status).to.eq(400)
+        expect(response.body.message).to.eq('ValidationError: \"password\" is required')
+      });
+    });
   })
 })
--- projects/mark85-cypress-api/cypress/support/commands.js
+++ projects/mark85-cypress-api/cypress/support/commands.js
@@ -1,8 +1,18 @@
+// commands.js: Definindo um novo comando personalizado chamado "postUser" para realizar uma requisição POST para a rota "/users"
+// No arquivo support/commands.js, um comando personalizado é adicionado ao Cypress
 Cypress.Commands.add("postUser", (user) => {
-    cy.api({
-      url: "/users",
-      method: "POST",
-      body: user,
-      failOnStatusCode: false,
-    }).then((response) => { return response })
-  });
+  // Uma requisição API é feita
+  cy.api({
+    // A URL para a requisição é definida
+    url: "/users",
+    // O método para a requisição é definido
+    method: "POST",
+    // O corpo da requisição é definido
+    body: user,
+    // Não falha no código de status
+    failOnStatusCode: false,
+  }).then(response => { 
+    // Retorna a resposta
+    return response 
+  })
+})
